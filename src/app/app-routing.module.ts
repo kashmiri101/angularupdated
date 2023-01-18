76f41db865df4603578bcd36404233bbc7c586a7
kashmiri101 <atharh1678@gmail.com>
2023-01-19 02:02:55 +0500

    angularupdated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {InventoryComponent} from './inventory/inventory.component';
+import {FaqComponent} from './faq/faq.component';
+import {TestimonialComponent} from './testimonial/testimonial.component';
+import {PrivacypolicyComponent} from './privacypolicy/privacypolicy.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',  
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
 
-const routes: Routes = [];
+  {
+    path: 'header',
+    component: HeaderComponent
+  },
+  {
+    path: 'footer',
+    component: FooterComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
+  }, 
+  {
+    path: 'faq',
+    component: FaqComponent
+  },
+  {
+    path: 'testimonial',
+    component:TestimonialComponent
+  },
+  {
+    path: 'privacypolicy',
+    component:PrivacypolicyComponent
+  }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,61 @@
+
+
+import { Component, OnInit } from '@angular/core';
+import {  faMailForward, faCircleCheck, faArrowRightLong, faF, faHeart, faPlay, faCompress, faArrowLeft, faArrowRight, faUser, faCarSide, faLocationDot, faEnvelope, faCaretRight, faXmark, faPlus, faHandPointer, faCartPlus, faStar, faPhone, faLocation, faMailBulk } from '@fortawesome/free-solid-svg-icons';
+import { faApple, faGooglePlay, faWhatsapp, faLinkedin, faInstagram,  faTwitter, faFacebook, faGoogle, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent  implements OnInit {
+  faMailForward=faMailForward;
+  faArrowRightLong=faArrowRightLong;
+  faFa=faF;
+  faPlay=faPlay;
+  faHeart=faHeart;
+  faCompress=faCompress;
+  faApple=faApple;
+  faGooglePlay=faGooglePlay;
+  faArrowRight=faArrowRight;
+  faArrowLeft=faArrowLeft;
+  faUser=faUser;
+  faHandPointer=faHandPointer;
+  faCartPlus=faCartPlus;
+  faStar=faStar;
+  faPhone=faPhone;
+  faLocation=faLocation; 
+  faMailBulk=faMailBulk;
+  faPlus=faPlus;
+  faXmark=faXmark;
+  faFacebook=faFacebook;
+  faTwitter=faTwitter;
+  faYoutube=faYoutube;
+  faInstagram=faInstagram;
+  faLinkedin=faLinkedin;
+  faGoogle=faGoogle;
+  faCaretRight=faCaretRight;
+  faWhatsapp=faWhatsapp;
+  faEnvelope=faEnvelope;
+  faLocationDot=faLocationDot;
+  faCarSide=faCarSide;
+  faCircleCheck=faCircleCheck;
+  //flaticonspeedometer=flaticonspeedometer;
+
+  constructor (private router: Router ){}
+
+  ngOnInit(): void {
+  }
+
+  goHome(){
+    this.router.navigate(['home']);
+  }
+
+  goAbout(){
+    this.router.navigate(['about']);
+  }
+}
+
